19c864aa14c4abf57cccb66652c4d81f01a68ccf
EhsanulHaqueSiam <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2023-05-17 04:03:57 +0600

    Update UCheckOut.java

--- myClasses/UCheckOut.java
+++ myClasses/UCheckOut.java
@@ -220,11 +220,6 @@ public class UCheckOut extends JFrame implements ActionListener, myInterface.Che
         deleteRoomEntry(/*nameLogin, roomNum_combo*/);
         roomNum_combo.setSelectedIndex(-1);
 
-//        search_btn = new JButton("Search");
-//        search_btn.setBounds(491, 32, 89, 23);
-//        search_btn.setFocusable(false);
-//        contentPane.add(search_btn);
-
         clear_btn = new JButton("Clear");
         clear_btn.setBounds(160, 284, 97, 23);
         clear_btn.setFocusable(false);
@@ -235,7 +230,6 @@ public class UCheckOut extends JFrame implements ActionListener, myInterface.Che
         checkOut_btn.addActionListener(this);
         clear_btn.addActionListener(this);
         roomNum_combo.addActionListener(this);
-        //search_btn.addActionListener(this);
 
         this.setVisible(true);
 
