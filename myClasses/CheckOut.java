02bfa3fd06896238c529dc776527571f1b50ab41
EhsanulHaqueSiam <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2023-05-17 04:00:19 +0600

    Update CheckOut.java

--- myClasses/CheckOut.java
+++ myClasses/CheckOut.java
@@ -202,11 +202,6 @@ public class CheckOut extends JFrame implements ActionListener, myInterface.Clea
         roomSearch();
         search_combo.setSelectedIndex(-1);
 
-//        search_btn = new JButton("Search");
-//        search_btn.setBounds(491, 32, 89, 23);
-//        search_btn.setFocusable(false);
-//        contentPane.add(search_btn);
-
         clear_btn = new JButton("Clear");
         clear_btn.setBounds(160, 284, 97, 23);
         clear_btn.setFocusable(false);
@@ -217,7 +212,6 @@ public class CheckOut extends JFrame implements ActionListener, myInterface.Clea
         checkOut_btn.addActionListener(this);
         clear_btn.addActionListener(this);
         search_combo.addActionListener(this);
-        //search_btn.addActionListener(this);
 
         this.setVisible(true);
 
