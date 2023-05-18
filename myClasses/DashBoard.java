0e739095e6f09a13289f86df9dbc7eea7c03e64b
NitPaul <aonyendopaul@gmail.com>
2023-05-19 02:36:59 +0600

    Tabel design have changed and Color added

--- myClasses/DashBoard.java
+++ myClasses/DashBoard.java
@@ -3,7 +3,9 @@ package myClasses;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -54,6 +56,27 @@ public class DashBoard extends JFrame implements ActionListener {
         table.setEnabled(false);
 
         model.setRowCount(0);
+
+        // Set custom background colors for alternate rows
+        table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                if (row % 2 == 0) {
+                    component.setBackground(new Color(230, 230, 230)); // Light gray for even rows
+                } else {
+                    component.setBackground(Color.WHITE); // White for odd rows
+                }
+                return component;
+            }
+        });
+
+// Customize table header names
+        JTableHeader header = table.getTableHeader();
+        header.setBackground(new Color(150, 150, 150)); // Dark gray for header background
+        header.setForeground(Color.WHITE); // White text color for header
+        Font headerFont = header.getFont();
+        header.setFont(headerFont.deriveFont(Font.BOLD)); // Make the font bold
 
 
         try (BufferedReader br = new BufferedReader(new FileReader("./files/rooms.txt"))) {
